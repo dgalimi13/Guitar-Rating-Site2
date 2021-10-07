d0fb7d594257af031efbb36a5a0ab8686bf89129
danny galimi <dannygalimi13@aim.com>
2021-10-07 13:06:16 -0400

    altered user relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :guitars, through :reviews
+    has_many :reviewed_guitars, through: :reviews, source: :guitar
+
+    has_many :guitars
 end
