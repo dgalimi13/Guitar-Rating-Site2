c1df7768b55bcf0eca260a04cd9556c5ec34590b
danny galimi <dannygalimi13@aim.com>
2021-10-07 12:49:02 -0400

    added user has_many relationships

---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_06_200511) do
+
+  create_table "guitars", force: :cascade do |t|
+    t.string "model"
+    t.string "description"
+    t.integer "make_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["make_id"], name: "index_guitars_on_make_id"
+    t.index ["user_id"], name: "index_guitars_on_user_id"
+  end
+
+  create_table "makes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "stars"
+    t.string "title"
+    t.string "content"
+    t.integer "user_id", null: false
+    t.integer "guitar_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["guitar_id"], name: "index_reviews_on_guitar_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "guitars", "makes"
+  add_foreign_key "guitars", "users"
+  add_foreign_key "reviews", "guitars"
+  add_foreign_key "reviews", "users"
+end
