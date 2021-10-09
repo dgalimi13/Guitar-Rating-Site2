38562dddf7dda9ad4ff3ec5b998e3920becd40e4
danny galimi <dannygalimi13@aim.com>
2021-10-09 17:11:17 -0400

    wrote custom writer for make attributes

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -4,5 +4,8 @@ class Guitar < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
-  accepts_nested_attributes_for :make
+  def make_attributes=(attributes)
+    make = Make.find_or_create_by(attributes) if !name.empty?
+  end 
+  
 end
