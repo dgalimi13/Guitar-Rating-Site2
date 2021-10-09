00d2cee731bb204a91cfac3a6bbd760e4a47aa77
danny galimi <dannygalimi13@aim.com>
2021-10-09 17:52:31 -0400

    added to review new action, built basic review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,13 @@
 class ReviewsController < ApplicationController
 
     def new
+        if @guitar = Guitar.find_by_id(params[:id])
+        @review = @guitar.reviews.build 
+        else
+            @review = Review.new
     end 
 
     def index
     end 
-    
+
 end
