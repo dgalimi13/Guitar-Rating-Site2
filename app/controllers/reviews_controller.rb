369230a64c6da64eb34c2907e7cc5b9bffe1107e
danny galimi <dannygalimi13@aim.com>
2021-10-13 10:11:59 -0400

    added header and number field to stars under new review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,11 @@
 class ReviewsController < ApplicationController
 
     def new
-        if @guitar = Guitar.find_by_id(params[:id])
+        if @guitar = Guitar.find_by_id(params[:guitar_id])
         @review = @guitar.reviews.build 
         else
             @review = Review.new
+        end 
     end 
 
     def index
