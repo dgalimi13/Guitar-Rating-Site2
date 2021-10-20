d6856a4058900eed9d5a5eaf665bb7444958fd54
danny galimi <dannygalimi13@aim.com>
2021-10-20 13:12:15 -0400

    created display_header method to reviews_helpers and added it to review new form

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
+
+    def display_header(review)
+        if params[:guitar_id]
+            content_tag(:h1, "Add a Review for #{review.guitar.model} -  #{review.guitar.make.name}")
+        else
+          content_tag(:h1, "Create a review")
+        end
+      end
 end
