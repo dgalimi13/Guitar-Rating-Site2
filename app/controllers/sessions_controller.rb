64547671313d997fdb4b70ec9a450d353c17e454
danny galimi <dannygalimi13@aim.com>
2021-10-21 14:38:59 -0400

    added omniauth and auth methods to sessions controller, altered sessions create to handle google logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,16 +11,36 @@ class SessionsController < ApplicationController
     end 
 
     def create
+
+        if params[:provider] == 'google_oauth2'
+            @user = User.create_by_google_omniauth(auth)
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+
+        else 
+
+
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user.id 
-            redirect_to user_path(@user)
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
         else
-            flash[:error] = "Sorry, login info was incorrect, please try again"
+            flash[:error] = "Sorry, Login info was incorrect. Please try again"
             redirect_to login_path
-        end 
+        end
+      end 
+    end
 
-    end 
+    def omniauth
+        @user = User.create_by_google_omniauth(auth)
+
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+    end
+
+    def auth
+        request.env['omniauth.auth']
+      end
 
 end
  
